293bef30e76d515f31022e45090060c54290df62
QueueBert <quinn.williams@gmail.com>
2024-06-15 13:52:06 -0400

    testing exittime animations off the same state

--- BouquetOfTime/Assets/Scripts/States/RootMotionState.cs
+++ BouquetOfTime/Assets/Scripts/States/RootMotionState.cs
@@ -14,6 +14,6 @@ public class RootMotionState : PlayerState
     public override void PhysicsUpdate()
     {
         rb.transform.position += animator.deltaPosition;
-        rb.rotation = animator.deltaRotation * rb.rotation;
+        //rb.rotation = animator.deltaRotation * rb.rotation;
     }
 }
